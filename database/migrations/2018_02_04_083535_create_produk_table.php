6cb964a049c250466ff21660903cb9789f5d1e94
Hevn <handikahevn24@gmail.com>
2018-02-05 08:45:17 +0700

    Tambah Style Bootstrap

---
+++ database/migrations/2018_02_04_083535_create_produk_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdukTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('produk', function (Blueprint $table) {
+            //
+            $table->increments('id');
+            $table->string('nama_produk');
+            $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('produk');
+    }
+}
